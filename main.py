708567ae31a08a1e52a1856717d83111e18d6901
Cdavidson123 <cdavidson@connect.kellettschool.com>
2024-11-06 04:46:40 +0000

    save

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-# 
+from random import randint 
+
+correct = randint(0,100)
+number = None
+while number != correct:
+    print("Guess my number")
+    number = int(input())
+
+    if number > correct:
+        print("LOWER!")
+
+    if number < correct:
+        print("HIGHER!")
+
+    if number == correct:
+        print("YOU GOT IT!")
+
+    fiveless = correct - 5
+    fivemore = correct + 5
+
+    if number >= fiveless and number <= fivemore:
+        print("ALMOST! WITHIN 5 AWAY!")
